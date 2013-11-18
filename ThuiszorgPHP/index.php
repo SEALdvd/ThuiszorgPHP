16f06787f1fdbccc441d87068ea483a18680c2ce
SEALdvd <danny_vandalen@hotmail.com>
2013-11-18 09:03:48 +0100

    Simpel start
    Een simpele start

---
+++ ThuiszorgPHP/index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Danny van Dalen
+ * Date: 18-11-13
+ * Time: 8:59
+ * To change this template use File | Settings | File Templates.
+ */
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+ <h1>Hallo</h1>
+</body>
+</html>
